77cea742820be5d2e38b4c4342a86077439f7c3b
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-03-10 11:34:30 +0300

    Update isPrime.js
    Add a solution to the challenge

---
+++ JavaScript/isPrime/isPrime.js
@@ -0,0 +1,15 @@
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+console.log(isPrime(7)); // Should return true
+console.log(isPrime(14)); // Should return false
