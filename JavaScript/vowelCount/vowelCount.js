9c4b98191617424f68bb791e7fe04c09fa1785b2
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-03-10 11:44:55 +0300

    Update vowelCount.js
    Add solution

---
+++ JavaScript/vowelCount/vowelCount.js
@@ -0,0 +1,7 @@
+function countVowels(str) {
+  const vowels = /[aeiou]/gi;
+  const vowelCount = (str.match(vowels) || []).length;
+  return vowelCount;
+}
+console.log(countVowels("Hello, World!")); // Should return 3 (e, o, o)
+console.log(countVowels("JavaScript is awesome")); // Should return 8 (a, i, e, a, i, o, e, e)
