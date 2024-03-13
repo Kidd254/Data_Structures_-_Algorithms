169341f9666cf300ab9ebf69e1df6c3869fe7ff1
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-03-13 16:32:49 +0300

    Update vowelCount.js

--- JavaScript/vowelCount/vowelCount.js
+++ JavaScript/vowelCount/vowelCount.js
@@ -3,5 +3,5 @@ function countVowels(str) {
   const vowelCount = (str.match(vowels) || []).length;
   return vowelCount;
 }
-console.log(countVowels("Hello, World!")); // Should return 3 (e, o, o)
-console.log(countVowels("JavaScript is awesome")); // Should return 8 (a, i, e, a, i, o, e, e)
+console.log(countVowels("Hello, World!"));  // Should return 3 (e, o, o)
+console.log(countVowels("JavaScript is awesome"));  // Should return 8 (a, i, e, a, i, o, e, e)
